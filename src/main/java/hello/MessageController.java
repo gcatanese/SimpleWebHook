e535800a2498ca16bbc7c1826c835fefd2351ef9
Beppe Catanese <Giuseppe.Catanese@sapienzaconsulting.com>
2017-10-23 22:25:19 +0200

    random jokes

--- src/main/java/hello/MessageController.java
+++ src/main/java/hello/MessageController.java
@@ -1,5 +1,6 @@
 package hello;
 
+import java.util.Random;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,11 +21,36 @@ public class MessageController {
     private Messages[] getJoke1() {
         Messages[] messages = new Messages[1];
 
-        messages[0] = new Messages("Did you hear about the restaurant on the moon?");
-        //messages[1] = new Messages("Great food, no atmosphere.");
+        messages[0] = new Messages(getRandomElement());
 
         return messages;
     }
     
-    
+    private String getRandomElement() {
+        int randomInt = new Random().nextInt(arrayStrings.length);
+        
+        return arrayStrings[randomInt];
+    }
+
+    private String[] arrayStrings = {
+        "Farts have been clocked at a speed of 10 feet per second.",
+        "Donkeys kill more people annually than plane crashes.",
+        "The longest recorded flight of a chicken is thirteen seconds.",
+        "40,000 Americans are injured by toilets each year.",
+        "Banging your head against a wall uses 150 calories a hour.",
+        "Vending Machines Kill More People per year than sharks.",
+        "There are three golf balls sitting on the moon!",
+        "Like fingerprints, everyone's tongue print is different.",
+        "Butterflies taste with their feet.",
+        "In France, it is legal to marry a dead person.",
+        "To produce a single pound of honey, a single bee would have to visit 2 million flowers.",
+        "You are born with 300 bones, by the time you are an adult you will have 206.",
+        "A cockroach will live for weeks without its head before it starves to death",
+        "A pig's orgasm lasts 30 minutes.",
+        "No word in the English language rhymes with month, orange, silver or purple.",
+        "In Tokyo, a bicycle is faster than a car for most trips of less than 50 minutes!",
+        "An ostrich’s eye is bigger than its brain",
+        "Every year, kids in North America spend close to half a billion dollars on chewing gum!",
+        "Ketchup was sold in the 1830s as medicine."};
+
 }
